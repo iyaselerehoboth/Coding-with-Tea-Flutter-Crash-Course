523a5ef6de4e2988b1fc7cd552cf27173807707a
Iyasele Rehoboth <rehobothi@yahoo.com>
2023-05-23 23:28:19 +0100

    Initial commit: Finished video 1.5 of playlist.

---
+++ android/app/src/main/kotlin/com/iyaselerehoboth/coding_with_tea_flutter_crash_course/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.iyaselerehoboth.coding_with_tea_flutter_crash_course
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'screens/Dashboard.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Flutter Crash Course',
+      themeMode: ThemeMode.system,
+      debugShowCheckedModeBanner: false,
+      home: Dashboard()
+    );
+  }
+}
