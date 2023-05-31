925836a14bb7f95aa1cbd90d9c58a0e00964ceb6
Iyasele Rehoboth <rehobothi@yahoo.com>
2023-05-31 20:05:43 +0100

    Section 2.5: Text button with how to disable a button

--- lib/screens/Home.dart
+++ lib/screens/Home.dart
@@ -7,24 +7,29 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: ElevatedButton.icon(
+        child: TextButton(
+          //To disable a button, pass "null" in the onPressed property.
           onPressed: () {},
           style: ElevatedButton.styleFrom(
             padding: const EdgeInsets.all(20.0),
             fixedSize: const Size(300, 80),
             textStyle:
                 const TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-            primary: Colors.yellow,
+            //primary: Colors.yellow,
             onPrimary: Colors.black87,
-            elevation: 15,
+            //elevation: 15,
             shadowColor: Colors.yellow,
-            side: const BorderSide(color: Colors.black87, width: 2),
+            //side: const BorderSide(color: Colors.black87, width: 2),
             //alignment: Alignment.bottomLeft,
             shape: const StadiumBorder()
           ),
-          //child: const Text("Let's Begin"),
-          icon: const Icon(Icons.add_shopping_cart_outlined),
-          label: const Text("Let's Begin"),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              Text("Let's Begin"),
+              Icon(Icons.add_shopping_cart_outlined),
+            ],
+          ),
         ),
       ),
     );
